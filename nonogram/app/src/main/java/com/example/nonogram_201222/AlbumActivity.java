3c2dc70e110bf6b65036139e2b4588e4307f9d0a
Sooyong Kim <kst2575@naver.com>
2020-12-24 20:28:35 +0900

    201224_nonogram 레이아웃

---
+++ nonogram/app/src/main/java/com/example/nonogram_201222/AlbumActivity.java
@@ -0,0 +1,14 @@
+package com.example.nonogram_201222;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlbumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album);
+    }
+}
