a0c3bf62f4a04489ac48cbf8ccf48ebeb57f158d
Sooyong Kim <kst2575@naver.com>
2020-12-27 01:56:52 +0900

    intent

--- nonogram/app/src/main/java/com/example/nonogram_201222/MainActivity.java
+++ nonogram/app/src/main/java/com/example/nonogram_201222/MainActivity.java
@@ -2,14 +2,34 @@ package com.example.nonogram_201222;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton startBtn, albumBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        startBtn = findViewById(R.id.startBtn);
+        albumBtn = findViewById(R.id.albumBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, StageActivity.class);
+                startActivity(intent);
+            }
+        });
+        albumBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AlbumActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
