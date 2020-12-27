c4d6a0a49231bda2fbc5359d2eb2756bac75c6bc
Sooyong Kim <kst2575@naver.com>
2020-12-27 17:46:48 +0900

    201227 custom list view

---
+++ nonogram/app/src/main/java/com/example/nonogram_201222/CustomAdapter.java
@@ -0,0 +1,53 @@
+package com.example.nonogram_201222;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.HashMap;
+
+public class CustomAdapter  extends ArrayAdapter<HashMap<Integer, String>> {
+    private HashMap<Integer, String> items;
+    Context context;
+
+    public CustomAdapter(@NonNull Context context, int resource, HashMap<Integer, String> items){
+        super(context, resource);
+        this.items = items;
+        this.context = context;
+    }
+
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = inflater.inflate(R.layout.list_item_view, null, true);
+
+        //title
+        TextView title = convertView.findViewById(R.id.title_view);
+        title.setText(items.get(position).toString());
+
+        //icon
+        ImageView icon = convertView.findViewById(R.id.icon_view);
+        Context context = icon.getContext();
+        int id = context.getResources().getIdentifier("a" + position, "drawable",
+                context.getPackageName());
+        icon.setImageResource(id);
+
+        return convertView;
+    }
+
+
+}
--- nonogram/app/src/main/java/com/example/nonogram_201222/StageActivity.java
+++ nonogram/app/src/main/java/com/example/nonogram_201222/StageActivity.java
@@ -6,16 +6,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ListView;
+
+import java.util.HashMap;
 
 public class StageActivity extends AppCompatActivity {
 
     ImageButton intentBtn;
+
+    private HashMap<Integer, String> items = new HashMap<Integer, String>();
+    private ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stage);
 
         intentBtn = findViewById(R.id.intentBtn);
+
+        listView = findViewById(R.id.stageArea);
+
+        for(int i = 0;i<5;i++){
+            items.put(i, "item Number" + i);
+        }
+        System.out.println(items.values());
+
+        CustomAdapter adapter = new CustomAdapter(this, 0, items);
+        listView.setAdapter(adapter);
 
         intentBtn.setOnClickListener(new View.OnClickListener() {
             @Override
