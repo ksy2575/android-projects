a8fd43b3e1e09c9e466b71b59632d30f76888b5e
Sooyong Kim <kst2575@naver.com>
2021-01-18 22:34:11 +0900

    21.01.18 사각형 그리는 중

--- nonogram/app/src/main/java/com/example/nonogram_201222/GameView.java
+++ nonogram/app/src/main/java/com/example/nonogram_201222/GameView.java
@@ -10,14 +10,18 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class GameView extends View {
 
     private Paint paint = new Paint();
     private Path path = new Path();
     private int x,y;
-    int numberLength, squareLength;
+    int numberLength, squareLength, pointLength;
+
+    float[] pts = new float[22];
 
     public GameView(Context context) {
         super(context);
@@ -40,23 +44,34 @@ public class GameView extends View {
         Log.d("asdf", w + ", " + h);
         numberLength = (int)(Math.min(w,h)*0.25);
         squareLength = (int)(Math.min(w,h)*0.75);
-
+        pointLength = squareLength/10;
     }
-
     @Override
     protected void onDraw(Canvas canvas) {
-
         paint.setColor(Color.BLACK);
-
         //STROKE속성을 이용하여 테두리...선...
         paint.setStyle(Paint.Style.STROKE);
-
         //두께
         paint.setStrokeWidth(3);
-
-
         //path객체가 가지고 있는 경로를 화면에 그린다...
-        canvas.drawPath(path,paint);
+//        canvas.drawPath(path,paint);
+
+        //기본 구조 그리기
+
+        //가로줄
+        for(int i=0;i<22;i+=2){
+            pts[i] = ((float)(pointLength*i));
+            pts[i+1] = 0;
+            Log.d("asdf", pts[i] + " , " + pts[i+1]);
+        }
+        //세로줄
+//        for(int i=0;i<10;i++){
+//            pts[i] = ((float)(pointLength*i));
+//        }
+
+
+//        float[] pts={0,0,1080,1046,0,1046,1080,0};
+        canvas.drawLines(pts, paint);
 
     }
 
